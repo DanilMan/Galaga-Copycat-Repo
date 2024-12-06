298d59cc9cb2f628a06d4f7d49bd83e073446af8
Daniel Ruderman <rudermandaniel@gmail.com>
2024-12-05 21:01:29 -0800

    Added Enemy Spawner

--- Galaga Copycat/Assets/Scripts/Enemy/EnemyArray.cs
+++ Galaga Copycat/Assets/Scripts/Enemy/EnemyArray.cs
@@ -1,18 +1,23 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
 
 public class EnemyArray : MonoBehaviour
 {
     public GameObject[] enemies;
+    public EnemySpawnManager EnemySpawner;
     public int gridX;
     public int gridY;
     public float gridSpacingOffSet = 1f;
     public float gridOriginQuadrantX = 2f;
     public float gridOriginQuadrantY = 4f;
     private Vector2 gridOrigin;
+    private EnemySpawnManager[,] ESMs;
 
     private void Start()
     {
         gridOrigin = new Vector2((gridX - 1) * gridSpacingOffSet / gridOriginQuadrantX, (gridY - 1) * gridSpacingOffSet / gridOriginQuadrantY); // get centered origin
+        ESMs = new EnemySpawnManager[gridX, gridY];
         SpawnGrid();
     }
 
@@ -23,14 +28,20 @@ public class EnemyArray : MonoBehaviour
             for (int y = 0; y < gridY; y++)
             {
                 Vector3 spawnPosition = new Vector2(x * gridSpacingOffSet, y * gridSpacingOffSet) - gridOrigin;
-                PickAndSpawn(spawnPosition, Quaternion.identity);
+                SpawnEnemyManager(spawnPosition, Quaternion.identity, x, y);
+                //PickAndSpawn(spawnPosition, Quaternion.identity);
             }
         }
     }
 
-    private void PickAndSpawn(Vector3 spawnPosition, Quaternion rotation)
+    private void SpawnEnemyManager(Vector2 spawnPosition, Quaternion rotation, int x, int y)
     {
-        int randomIndex = Random.Range(0, enemies.Length);
-        GameObject clone = Instantiate(enemies[randomIndex], spawnPosition, rotation);
+        ESMs[x,y] = Instantiate(EnemySpawner, spawnPosition, rotation);
     }
+
+    //private void PickAndSpawn(Vector2 spawnPosition, Quaternion rotation)
+    //{
+    //    int randomIndex = Random.Range(0, enemies.Length);
+    //    GameObject clone = Instantiate(enemies[randomIndex], spawnPosition, rotation);
+    //}
 }
---
+++ Galaga Copycat/Assets/Scripts/Enemy/EnemySpawnManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    //private void PickAndSpawn(Vector3 spawnPosition, Quaternion rotation)
+    //{
+    //    int randomIndex = Random.Range(0, enemies.Length);
+    //    GameObject clone = Instantiate(enemies[randomIndex], spawnPosition, rotation);
+    //}
+}
